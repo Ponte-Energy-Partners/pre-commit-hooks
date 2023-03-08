26d01ac3e38a75036df8d8061946051d9d2b576d
Lorenz Walthert <lorenz.walthert@icloud.com>
2023-03-08 14:42:27 +0100

    test(verify-git-email): swap context manager for fixture

--- tests/core_test.py
+++ tests/core_test.py
@@ -1,4 +1,3 @@
-import contextlib
 import os
 import subprocess
 import tempfile
@@ -8,7 +7,7 @@ import pytest
 import pep_pre_commit_hooks.core as core
 
 
-@contextlib.contextmanager
+@pytest.fixture
 def temp_git_dir():
     with tempfile.TemporaryDirectory() as tmp:
         cwd = os.getcwd()
@@ -17,18 +16,15 @@ def temp_git_dir():
             subprocess.check_call(("git", "init"))
             yield
         finally:
-            # Change the directory back to home on any occasion
             os.chdir(cwd)
 
 
-def test_verify_domain():
-    with temp_git_dir():
-        subprocess.check_call(("git", "config", "user.email", "test@icloud.com"))
-        assert core.verify_git_email("icloud.com") is None
+def test_verify_domain(temp_git_dir):
+    subprocess.check_call(("git", "config", "user.email", "test@icloud.com"))
+    assert core.verify_git_email("icloud.com") is None
 
 
-def test_failed_verify_domain():
-    with temp_git_dir():
-        subprocess.check_call(("git", "config", "user.email", "test@me.com"))
-        with pytest.raises(ChildProcessError, match="but an e-mail address matching "):
-            core.verify_git_email("icloud.com")
+def test_failed_verify_domain(temp_git_dir):
+    subprocess.check_call(("git", "config", "user.email", "test@me.com"))
+    with pytest.raises(ChildProcessError, match="but an e-mail address matching "):
+        core.verify_git_email("icloud.com")
