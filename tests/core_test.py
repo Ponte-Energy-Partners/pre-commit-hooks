b6d1a4ca2127ec912ad8dac080d868218552e802
Lorenz Walthert <lorenzwalthert@users.noreply.github.com>
2023-03-09 12:21:11 +0100

    test(verify-git-email): use fixture for configuring git user

--- tests/core_test.py
+++ tests/core_test.py
@@ -19,12 +19,17 @@ def temp_git_dir():
             os.chdir(cwd)
 
 
-def test_verify_domain(temp_git_dir):
-    subprocess.check_call(("git", "config", "user.email", "test@icloud.com"))
+@pytest.fixture
+def config_git_user():
+    return lambda email: subprocess.check_call(("git", "config", "user.email", email))
+
+
+def test_verify_domain(temp_git_dir, config_git_user):
+    config_git_user("test@icloud.com")
     assert core.verify_git_email("icloud.com") is None
 
 
-def test_failed_verify_domain(temp_git_dir):
-    subprocess.check_call(("git", "config", "user.email", "test@me.com"))
+def test_failed_verify_domain(temp_git_dir, config_git_user):
+    config_git_user("test@me.com")
     with pytest.raises(ChildProcessError, match="but an e-mail address matching "):
         core.verify_git_email("icloud.com")
