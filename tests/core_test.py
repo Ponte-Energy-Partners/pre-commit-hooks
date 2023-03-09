78015226342b59c6f6c6b13ad3f6be422644c759
Lorenz Walthert <lorenzwalthert@users.noreply.github.com>
2023-03-09 16:11:59 +0100

    test(verify-git-email): better fixture cascading

--- tests/core_test.py
+++ tests/core_test.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-import tempfile
 
 import pytest
 
@@ -8,28 +7,32 @@ import pep_pre_commit_hooks.core as core
 
 
 @pytest.fixture
-def temp_git_dir():
-    with tempfile.TemporaryDirectory() as tmp:
-        cwd = os.getcwd()
-        try:
-            os.chdir(tmp)
-            subprocess.check_call(("git", "init"))
-            yield
-        finally:
-            os.chdir(cwd)
+def ch_tempdir(tmp_path):
+    """
+    Temporarily change the working directory to a temporary directory.
+    """
+    cwd = os.getcwd()
+    try:
+        os.chdir(tmp_path)
+        yield
+    finally:
+        os.chdir(cwd)
 
 
 @pytest.fixture
-def config_git_user():
-    return lambda email: subprocess.check_call(("git", "config", "user.email", email))
+def temp_git_dir(ch_tempdir):
+    subprocess.check_call(("git", "init"))
 
 
-def test_verify_domain(temp_git_dir, config_git_user):
-    config_git_user("test@icloud.com")
+@pytest.fixture
+def temp_git_dir_icloud_email(temp_git_dir):
+    subprocess.check_call(("git", "config", "user.email", "test@icloud.com"))
+
+
+def test_verify_domain(temp_git_dir_icloud_email):
     assert core.verify_git_email("icloud.com") is None
 
 
-def test_failed_verify_domain(temp_git_dir, config_git_user):
-    config_git_user("test@me.com")
+def test_failed_verify_domain(temp_git_dir_icloud_email):
     with pytest.raises(ChildProcessError, match="but an e-mail address matching "):
-        core.verify_git_email("icloud.com")
+        core.verify_git_email("hotmail.com")
